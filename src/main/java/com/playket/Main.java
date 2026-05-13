464f025d595b7385329277366486920b3b09c0eb
alvaroValverde <alvaro.val.cr@gmail.com>
2026-05-13 20:11:36 +0200

    main

--- src/main/java/com/playket/Main.java
+++ src/main/java/com/playket/Main.java
@@ -1,15 +1,15 @@
 package com.playket;
 
-import com.playket.database.ConexionDB;
-import java.sql.Connection;
+import com.playket.controller.LoginController;
+import com.playket.view.VentanaLogin;
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        Connection con = ConexionDB.getConexion();
-        if (con != null) {
-            System.out.println("Conexión exitosa con la base de datos Playket");
-        } else {
-            System.out.println("Error al conectar con la base de datos");
-        }
+        SwingUtilities.invokeLater(() -> {
+            VentanaLogin ventana = new VentanaLogin();
+            new LoginController(ventana);
+            ventana.setVisible(true);
+        });
     }
 }
